7e799bc7a87081657c4a6f69c8548c463ea05b60
enjinerd <dev@ron.my.id>
2022-05-12 17:22:24 +0700

    feat : / endpoint for tugas web service

---
+++ data/product.js
@@ -0,0 +1,288 @@
+const product = {
+    "kategori_sepatu": {
+        "id_kategori": "001",
+        "nama_kategori": "Sepatu,",
+        "produk": [
+            {
+                "id": "0013",
+                "nama": "Adidas Neo",
+                "deskripsi": "Sepatu Neo Adidas Keren",
+                "harga": 350000,
+                "berat": 0.9,
+                "stok_tersedia": 12,
+                "attribut": [
+                    "merah",
+                    43,
+                    "kets"
+                ]
+            },
+            {
+                "id": "0213",
+                "nama": "Nike Airblast",
+                "deskripsi": "Sepatu Olahraga Nike Airblast Keren",
+                "harga": 580000,
+                "berat": 0.9,
+                "stok_tersedia": 8,
+                "attribut": [
+                    "biru magenta",
+                    42,
+                    "kets"
+                ]
+            },
+            {
+                "id": "0400",
+                "nama": "Puma Casual",
+                "harga": 476800,
+                "deskripsi": "Sepatu Casual Puma Wanita",
+                "berat": 0.9,
+                "stok_tersedia": 23,
+                "attribut": [
+                    "biru magenta",
+                    35,
+                    "canvas"
+                ]
+            },
+            {
+                "id": "0221",
+                "nama": "Nike Kenta II",
+                "deskripsi": "Sepatu Olahraga Nike Kenta II Tokyo",
+                "harga": 750000,
+                "berat": 0.9,
+                "stok_tersedia": 12,
+                "attribut": [
+                    "biru magenta",
+                    42,
+                    "kets"
+                ]
+            },
+            {
+                "id": "2312",
+                "nama": "Zero W",
+                "deskripsi": "Sepatu Dance Zero W Keren",
+                "harga": 512000,
+                "berat": 0.9,
+                "stok_tersedia": 12,
+                "attribut": [
+                    "biru magenta",
+                    42,
+                    "kets"
+                ]
+            },
+            {
+                "id": "0213",
+                "nama": "Nike Airblast",
+                "deskripsi": "Sepatu Olahraga Nike Airblast Keren",
+                "harga": 350000,
+                "berat": 0.9,
+                "stok_tersedia": 12,
+                "attribut": [
+                    "biru magenta",
+                    42,
+                    "kets"
+                ]
+            },
+            {
+                "id": "0213",
+                "nama": "Nike Mini",
+                "harga": 784000,
+                "deskripsi": "Sepatu Olahraga Nike Mini Keren",
+                "berat": 0.9,
+                "stok_tersedia": 8,
+                "attribut": [
+                    "biru magenta",
+                    42,
+                    "kets"
+                ]
+            },
+            {
+                "id": "0400",
+                "nama": "Puma Casual",
+                "deskripsi": "Sepatu Casual Puma Wanita",
+                "berat": 0.9,
+                "stok_tersedia": 23,
+                "attribut": [
+                    "biru magenta",
+                    35,
+                    "canvas"
+                ]
+            },
+            {
+                "id": "0221",
+                "nama": "Nike Airblast II",
+                "deskripsi": "Sepatu Olahraga Nike Airblast II Keren",
+                "harga": 435700,
+                "berat": 0.9,
+                "stok_tersedia": 43,
+                "attribut": [
+                    "biru magenta",
+                    42,
+                    "kets"
+                ]
+            },
+            {
+                "id": "0213",
+                "nama": "Nike Water Cool",
+                "deskripsi": "Sepatu Renang Nike",
+                "harga": 341280,
+                "berat": 0.9,
+                "stok_tersedia": 24,
+                "attribut": [
+                    "biru magenta",
+                    42,
+                    "kets"
+                ]
+            },
+            {
+                "id": "0213",
+                "nama": "Nike Airblast",
+                "deskripsi": "Sepatu Olahraga Nike Airblast Keren",
+                "harga": 567230,
+                "berat": 0.9,
+                "stok_tersedia": 33,
+                "attribut": [
+                    "biru magenta",
+                    42,
+                    "kets"
+                ]
+            }
+        ]
+    },
+    "kategori_jaket": {
+        "id_kategori": "002",
+        "nama_kategori": "Jaket,",
+        "produk": [
+            {
+                "id": "1102",
+                "nama": "Hoodie Holligan",
+                "deskripsi": "Hoodie Kece Badai",
+                "harga": 400500,
+                "berat": 0.9,
+                "stok_tersedia": 45,
+                "attribut": [
+                    "Hitam",
+                    "S/L/M/XL",
+                    "hoodie"
+                ]
+            },
+            {
+                "id": "0583",
+                "nama": "Artera Coach Jacket",
+                "deskripsi": "Coach Jacket Gan",
+                "harga": 412567,
+                "berat": 0.8,
+                "stok_tersedia": 14,
+                "attribut": [
+                    "biru magenta",
+                    "S/L/M/XL",
+                    "Coach Jacket"
+                ]
+            },
+            {
+                "id": "1122",
+                "nama": "Puma Winter",
+                "deskripsi": "Jaket Musim Dingin",
+                "harga": 478009,
+                "berat": 0.9,
+                "stok_tersedia": 56,
+                "attribut": [
+                    "merah",
+                    "L/XL/XXL",
+                    "coat"
+                ]
+            },
+            {
+                "id": "1123",
+                "nama": "Puma Summer",
+                "deskripsi": "Jaket Musim Panas",
+                "harga": 350000,
+                "berat": 0.9,
+                "stok_tersedia": 56,
+                "attribut": [
+                    "coklat",
+                    "L/XL/XXL",
+                    "coat"
+                ]
+            },
+            {
+                "id": "1124",
+                "nama": "Puma Autumn",
+                "deskripsi": "Jaket Musim Gugur",
+                "harga": 350000,
+                "berat": 0.9,
+                "stok_tersedia": 56,
+                "attribut": [
+                    "hijau",
+                    "L/XL/XXL",
+                    "coat"
+                ]
+            },
+            {
+                "id": "1125",
+                "nama": "Puma Spring",
+                "deskripsi": "Jaket Musim Semi",
+                "harga": 350000,
+                "berat": 0.9,
+                "stok_tersedia": 44,
+                "attribut": [
+                    "hijau",
+                    "L/XL/XXL",
+                    "coat"
+                ]
+            },
+            {
+                "id": "4646",
+                "nama": "Supreme Hype",
+                "deskripsi": "Jaket HypeBeast",
+                "harga": 5240000,
+                "berat": 0.9,
+                "stok_tersedia": 8,
+                "attribut": [
+                    "merah maroon",
+                    "S/M/XL",
+                    "hoodie"
+                ]
+            },
+            {
+                "id": "7688",
+                "nama": "Minimal Jacket Canvas",
+                "harga": 250988,
+                "deskripsi": "Jaket Pecinta Minimalis",
+                "berat": 0.9,
+                "stok_tersedia": 23,
+                "attribut": [
+                    "biru magenta",
+                    "S/M/L",
+                    "canvas"
+                ]
+            },
+            {
+                "id": "5642",
+                "nama": "Maximalist Jacket Canvas",
+                "harga": 1450000,
+                "deskripsi": "Jaket Pecinta Maksimalist",
+                "berat": 0.9,
+                "stok_tersedia": 23,
+                "attribut": [
+                    "ungu",
+                    "S/M/L",
+                    "canvas"
+                ]
+            },
+            {
+                "id": "3422",
+                "nama": "Ergonomist Jacket Canvas",
+                "harga": 487500,
+                "deskripsi": "Jaket Pecinta Ergonomist",
+                "berat": 0.9,
+                "stok_tersedia": 23,
+                "attribut": [
+                    "hitam",
+                    "S/M/L",
+                    "canvas"
+                ]
+            }
+        ]
+    }
+}
+
+export default product;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const { SuccessResponseObject } = require('../common/http');
+import product   from "../data/product";
 const demo = require('./demo.route');
 
 const r = Router();
 
 r.use('/demo', demo);
 
-r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
+r.get('/', (req, res) => res.json(product));
 
 module.exports = r;
